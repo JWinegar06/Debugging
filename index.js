70eeeda7e82cc23fbb1d803e64347f7bee023a16
JWinegar06 <winegaj86@gmail.com>
2025-04-02 16:28:31 -0600

    Updates

---
+++ index.js
@@ -0,0 +1,27 @@
+let message = ("Hello, World!");
+console.log(`${message}`); 
+
+
+let myVariable 
+console.log(myVariable); 
+
+
+const greeting = "Hello";
+console.log(greeting);
+
+
+let obj = {
+  property: "property"
+}
+console.log(obj.property); 
+
+
+const user = {
+  name: "John",
+  age: "30",
+}
+console.log("User info: " , user); 
+
+
+let num = 10;
+console.log(num + 5); 
